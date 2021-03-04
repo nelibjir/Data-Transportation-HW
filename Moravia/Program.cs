7255f5c15e1c9cc18f8b5db95aea5bb9a452bca9
Jiri Neliba <jiri.neliba@qest.cz>
2021-03-04 15:20:04 +0100

    Create settings json and class, tesing json, get rid of literals in Program, create DTO class

--- Moravia/Program.cs
+++ Moravia/Program.cs
@@ -1,52 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Xml.Linq;
+using Moravia.Dtos;
 using Newtonsoft.Json;
 
 namespace Moravia
 {
-    public class Document
-    {
-        public string Title { get; set; }
-        public string Text { get; set; }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// TODO check also it exists! 
+			string sourceFileName = Path.Combine(Environment.CurrentDirectory, Settings.GetSourceFilePath());
+			string targetFileName = Path.Combine(Environment.CurrentDirectory, Settings.GetDestinationFilePath());
 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            // TODO add this files here to some resources in app scope
-            // FIXME -> this should be in config file and not here, I mean the strings 
-            // TODO check also it exists! 
-            var sourceFileName = Path.Combine(Environment.CurrentDirectory, "..\\..\\..\\Source Files\\Document1.xml");
-            var targetFileName = Path.Combine(Environment.CurrentDirectory, "..\\..\\..\\Target Files\\Document1.json");
+			//FIXME put the reader to using, so the stream would be dispatch, closed
+			try
+			{
+				FileStream sourceStream = File.Open(sourceFileName, FileMode.Open); // TODO reduce - this line is needless
+				var reader = new StreamReader(sourceStream);
+				string input = reader.ReadToEnd();
 
-            //FIXME put the reader to using, so the stream would be dispatch, closed
-            try
-            {
-                FileStream sourceStream = File.Open(sourceFileName, FileMode.Open); // TODO reduce - this line is needless
-                var reader = new StreamReader(sourceStream);
-                string input = reader.ReadToEnd();
-            }
-            catch (Exception ex) // FIXME with exact Exceptions
-            {
-                throw new Exception(ex.Message); // FIXME on some exact exception, also we don't need to to throw it again maybe, this is now useless
-            }
 
-            // TODO should check this is really xml
-            var xdoc = XDocument.Parse(input); //FIXME the input is not seen on scope, should be above catch
-            var doc = new Document
-            {
-                Title = xdoc.Root.Element("title").Value, /// TODO what if title is null?
-                Text = xdoc.Root.Element("text").Value // TODO what if text is null?
-            };
+				// TODO should check this is really xml
+				var xdoc = XDocument.Parse(input); //FIXME the input is not seen on scope, should be above catch
+				var doc = new DocumentDto
+				{
+					Title = xdoc.Root.Element("title").Value, /// TODO what if title is null?
+					Text = xdoc.Root.Element("text").Value // TODO what if text is null?
+				};
 
-            var serializedDoc = JsonConvert.SerializeObject(doc);
+				var serializedDoc = JsonConvert.SerializeObject(doc);
 
-            var targetStream = File.Open(targetFileName, FileMode.Create, FileAccess.Write);
-            var sw = new StreamWriter(targetStream); //FIXME again all of this in the using together
-            sw.Write(serializedDoc); // FIXME under catch also! 
-        }
-    }
+				var targetStream = File.Open(targetFileName, FileMode.Create, FileAccess.Write);
+				var sw = new StreamWriter(targetStream); //FIXME again all of this in the using together
+				sw.Write(serializedDoc); // FIXME under catch also! 
+
+			}
+			catch (Exception ex) // FIXME with exact Exceptions
+			{
+				throw new Exception(ex.Message); // FIXME on some exact exception, also we don't need to to throw it again maybe, this is now useless
+			}
+		}
+	}
 }
---
+++ Moravia/Settings.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Moravia
+{
+	public class Settings
+	{
+        private static IConfigurationBuilder fBuilder;
+        public static string GetSourceFilePath() {
+            if (fBuilder == null)
+                createBuilder();
+            return fBuilder.Build().GetSection("SourceFile").Value;
+        }
+
+        public static string GetDestinationFilePath() {
+            if (fBuilder == null)
+                createBuilder();
+            return fBuilder.Build().GetSection("DestinationFile").Value;
+        }
+
+        private static void createBuilder()
+        {
+			fBuilder = new ConfigurationBuilder()
+                                .SetBasePath(Directory.GetCurrentDirectory())
+                                .AddJsonFile("appsetings.json", optional: true, reloadOnChange: true);
+        }
+    }
+}
